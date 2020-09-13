ab9f71caba13f40e5932c1c2021ea2cb7add4a53
johncooper87 <johnxcooper87@gmail.com>
2020-09-13 11:51:39 +0700

    very last update

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -26,14 +26,14 @@ const innerClick1 = () => setTimeout(innerClick, 0);
 
 function App() {
   console.log('App');
-  const lalala = useObservable(observable1);
-  const bebebe = useObservable(observable2);
+  const asdasd = useObservable(observable1);
+  const zxczxc = useObservable(observable2);
 
   return <div style={{ padding: '24px', backgroundColor: 'yellow', width: '96px' }} onClick={outerClick1} >
     <div style={{ padding: '12px', backgroundColor: 'red' }} onClick={innerClick1} >
-      num1: {lalala}
+      num1: {asdasd}
       <br />
-      num2: {bebebe}
+      num2: {zxczxc}
     </div>
   </div>;
 }
