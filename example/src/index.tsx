252016c7b6b5221dcf23bfa7350b286fa8a90e79
johncooper87 <ibondarev@ask-gps.ru>
2020-09-14 12:53:17 +0700

    tralala

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -26,12 +26,12 @@ const innerClick1 = () => setTimeout(innerClick, 0);
 
 function App() {
   console.log('App');
-  const ttt = useObservable(observable1);
+  const lalala111 = useObservable(observable1);
   const lll = useObservable(observable2);
 
   return <div style={{ padding: '24px', backgroundColor: 'yellow', width: '96px' }} onClick={outerClick1} >
     <div style={{ padding: '12px', backgroundColor: 'red' }} onClick={innerClick1} >
-      num1: {ttt}
+      num1: {lalala111}
       <br />
       num2: {lll}
     </div>
